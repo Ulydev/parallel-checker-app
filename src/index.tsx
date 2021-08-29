8a4469dc6adb4684f34f1ddf3b83ef4499d7ce1a
Ulydev <ulydev@live.fr>
2021-08-29 04:14:54 +0200

    add react-snap postbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { Web3Provider } from "@ethersproject/providers"
 import { Web3ReactProvider } from "@web3-react/core"
 import React from "react"
-import ReactDOM from "react-dom"
+import { hydrate, render } from "react-dom"
 import "react-grid-layout/css/styles.css"
 import ReactModal from "react-modal"
 import "react-resizable/css/styles.css"
@@ -14,12 +14,12 @@ ReactModal.setAppElement("#root")
 
 const getLibrary = (provider: any) => new Web3Provider(provider)
 
-ReactDOM.render(
-    /*<React.StrictMode>*/
+const rootElement = document.getElementById("root")
+;(rootElement?.hasChildNodes() ? hydrate : render)(
     <Web3ReactProvider getLibrary={getLibrary}>
         <App />
     </Web3ReactProvider>,
-    /*</React.StrictMode>*/ document.getElementById("root")
+    document.getElementById("root")
 )
 
 // If you want your app to work offline and load faster, you can change
